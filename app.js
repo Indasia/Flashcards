26c7480c945c2b2d5b73608d44dbc66c6607550d
Indasia Summerfield <Indasia@yahoo.com>
2019-03-30 21:47:03 -0700

    update

--- app.js
+++ app.js
@@ -8,22 +8,29 @@ app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-    res.render("index");
+const mainRoutes = require("./routes");
+const cardRoutes = require("./routes/cards");
+
+app.use(mainRoutes);
+app.use("/cards", cardRoutes);
+
+app.use((req, res, next) => {
+    console.log("Hello");
+    const err = new Error("Oh no!");
+    err.status = 500;
+    next(err);
 });
 
-app.get("/cards", (req, res) => {
-    res.render("card", {
-        prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is.", colors});
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
-app.get("/hello", (req, res) => {
-    res.render("hello", {name: req.cookies.username});
-});
-
-app.post("/hello", (req, res) => {
-    res.cookie("username", req.body.username);
-    res.render("hello", {name: req.body.username});
+app.use((err, req, res, next) => { 
+    res.locals.error = err;
+    res.status(err.status)
+    res.render("error");
 });
 
 app.listen(3000, () => {
